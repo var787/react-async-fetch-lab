557bdc468bbd4b2e9c0def8276046afd70b16e83
var787 <hegde.rakshan@gmail.com>
2021-01-17 20:55:49 -0600

    commit tree 26ed2ee2817307712d7e8b1954138892c499977c

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
-// create your App component here
+import React from 'react'
+
+export default class App extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            people: []
+        }
+    }
+
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+            .then(resp => resp.json())
+            .then(peopleData =>(
+                this.setState({
+                    people: peopleData
+                })
+            ))
+            
+        }
+    
+    
+    render(){
+        return (
+        <div>
+            <li>Hello</li>
+        </div>
+        )
+    }
+}
